fdaa05541507543e2f65fe250953ebb4ba0d93b5
qaioz <tabatadzegaga@gmail.com>
2025-02-17 23:26:28 +0400

    initial

---
+++ src/main/java/com/kiu/hackaton/hackathon_back/HackathonBackApplication.java
@@ -0,0 +1,13 @@
+package com.kiu.hackaton.hackathon_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonBackApplication.class, args);
+	}
+
+}
